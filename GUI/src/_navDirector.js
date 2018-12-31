0169446389cd839c69af555ffe4f96f0600cba4f
unknown <jerson.huayta@tecsup.edu.pe>
2018-12-31 09:31:07 -0500

    Intrfaces por usuarios

--- GUI/src/_navDirector.js
+++ GUI/src/_navDirector.js
@@ -1,1 +1,62 @@
-_navDirector.js
+export default {
+  items: [
+    
+    {
+      title: true,
+      name: 'Mantenimientos',
+      wrapper: {            // optional wrapper object
+        element: '',        // required valid HTML5 element tag
+        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+      },
+      class: ''             // optional class names space delimited list for title item ex: "text-center"
+    },
+    {
+      name: 'Registros',
+      url: '/Home/Registros',
+      icon: 'icon-pencil',
+    },
+    
+    {
+      name: 'Alumnos',
+      url: '/Home/Alumnos',
+      icon: 'icon-pencil',
+    },
+    {
+      name: 'Usuarios',
+      url: '/Home/Usuarios',
+      icon: 'icon-pencil',
+    },
+    {
+      name: 'Tutores',
+      url: '/Home/Tutores',
+      icon: 'icon-pencil',
+    },
+    {
+      title: true,
+      name: 'Reportes',
+      wrapper: {
+        element: '',
+        attributes: {},
+      },
+    },
+    {
+      name: 'Reporte de alumno',
+      url: '/base',
+      icon: 'icon-puzzle',
+      children: [
+        {
+          name: 'Por fecha',
+          url: '/base/breadcrumbs',
+          icon: 'icon-puzzle',
+        },
+        {
+          name: 'Busqueda de alumno',
+          url: '/base/cards',
+          icon: 'icon-puzzle',
+        },
+        
+      ],
+    },
+    
+  ],
+};
--- GUI/src/_navPadre.js
+++ GUI/src/_navPadre.js
@@ -1,1 +1,28 @@
-_navPadre.js
+export default {
+  items: [
+    
+    {
+      title: true,
+      name: 'Mantenimientos',
+      wrapper: {            // optional wrapper object
+        element: '',        // required valid HTML5 element tag
+        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+      },
+      class: ''             // optional class names space delimited list for title item ex: "text-center"
+    },
+    {
+      name: 'Por fecha',
+      url: '/base/breadcrumbs',
+      icon: 'icon-puzzle',
+    },
+    {
+      name: 'General',
+      url: '/Home/Colegios',
+      icon: 'icon-pencil',
+    },
+    
+  
+    
+    
+  ],
+};
