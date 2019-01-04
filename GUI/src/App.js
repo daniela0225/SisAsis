66c51adf1073fea888e2c5e14617834328e2abd2
unknown <jerson.huayta@tecsup.edu.pe>
2019-01-04 13:00:15 -0500

    Intrfaces por usuarios

--- GUI/src/App.js
+++ GUI/src/App.js
@@ -81,7 +81,7 @@ class App extends Component {
             <Route exact path="/register" name="Register Page" component={Register} />
             <Route exact path="/404" name="Page 404" component={Page404} />
             <Route exact path="/500" name="Page 500" component={Page500} />
-            <Route path="/" name="Home" component={AdminLayout} />
+            <Route path="/" name="Home" component={PadreLayout} />
           </Switch>
       </HashRouter>
     );
