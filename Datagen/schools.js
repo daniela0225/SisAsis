713aae65ef8346beca50abd330ad6a8fe1b0fa41
JuanPinoC <juan.pino@tecsup.edu.pe>
2019-02-18 10:58:47 -0500

    .

--- Datagen/schools.js
+++ Datagen/schools.js
@@ -2,7 +2,7 @@ db.schools.insert(
 {
         _id : ObjectId('5c5d9fe75cb286b5fae960a9'),
         name : "Colegio Ejemplo",
-        logo : "pato.png",
+        logo : "uploads\\default.jpeg",
         kinder : true,
         primary : true,
         highschool : true
